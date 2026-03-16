f9afe340c9294446d3a7c2b7bac2318e445be55a
Andreas Casen <andreas.casenarg@gmail.com>
2026-03-16 23:31:51 +0100

    add drizzle

--- dataSources/sqlite.ts
+++ dataSources/sqlite.ts
@@ -1,30 +1,21 @@
 import { Database } from 'bun:sqlite'
 import fs from 'node:fs/promises'
 import path from 'node:path'
+import { sql } from 'drizzle-orm'
+import { drizzle } from 'drizzle-orm/bun-sqlite'
+import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core'
+
 import { env } from '../env'
 
 const DB_PATH = path.join(env.DB_DIRECTORY, 'brain.db')
 
-let db: Database
+await fs.mkdir(env.DB_DIRECTORY, { recursive: true })
+const sqlite = new Database(DB_PATH)
+export const db = drizzle({ client: sqlite })
 
-export const initDb = async () => {
-	await fs.mkdir(env.DB_DIRECTORY, { recursive: true })
-
-	db = new Database(DB_PATH)
-
-	db.run(`
-        CREATE TABLE IF NOT EXISTS tasks (
-            id      TEXT PRIMARY KEY
-            title   TEXT NOT NULL
-            tags    TEXT NOT NULL DEFAILT '[]'
-            due     TEXT
-            description:    TEXT
-        )    
-    `)
-}
-
-const getDb = () => {
-	if (!db) throw new Error('Database not initialized - Call initDb()')
-
-	return db
-}
+const tasks = sqliteTable('tasks', {
+	id: text('id').primaryKey(),
+	title: text('title').notNull(),
+	tags: text('tags', { mode: 'json' }).$type<string[]>().default(sql`'[]'`),
+	description: text('description'),
+})
